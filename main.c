446a33c97d37ad574329853915893470d95e922a
Bananachu <bananachu.pokepedia@gmail.com>
2021-08-07 14:35:09 +0200

    test des clefs SSH n°2

--- main.c
+++ main.c
@@ -1,12 +1,43 @@
+/* CODE SOURCE DU "JEU" PROTOTYPE
+
+Auteur : Bananachu
+
+Pour plus d'informatios sur le projet, veuillez
+consulter le repo GitHub :
+https://www.github.com/Bananachu/prototype
+
+Remerciements spéciaux à Alex Allain du site
+https://www.cprogramming.com
+pour ses cours de C et de C++, ainsi qu'à
+la chaîne YouTube freeCodeCamp.org pour les 
+même raisons
+
+Ce code n'est publié sous aucune licence et est
+totalement libre, vous pouvez l'utiliser comme 
+bon vous semble, même si des crédits seraient
+appréciés. 
+
+Ce code a été écrit sur SublimeText et compilé
+avec GCC, merci aux dévloppeurs de ces deux 
+applications !*/
+
+
 #include <stdio.h>
 #include <string.h>
 
 
-char input[12]; //Cette variable stocke l'input de l'utilisateur
-int zone;       //Celle-là stocke la zone (cette valeur n'est pas montrée en jeu
-int sequel;			//mais est utilisée pour simuler les déplacements)
+// Variables cachées
+char input[12];	// Pour stocker l'input de l'utilisateur.
+int zone; // Pour stocker le numéro de zone.
+int sequel; // Pour stocker la progression des dialogues.
 
-//Fonction permettant de "purifier" l'input en supprimant le saut de ligne à la fin
+
+/*Fonctions "motrices"
+Ces fonctions sont appelées à de nombreuses reprises et
+servent au fonctionnement même du logiciel, elle ne représentent
+pas forcément quelque chose de concret pour l'utilisateur.*/
+
+// Supprime le \n à la fin de l'input
 void purify(char *str, int size) {
 	int i;
 	for (i = 0; i < size; i++) {
@@ -18,8 +49,9 @@ void purify(char *str, int size) {
 }
 
 
-//Fonction affichant la partie "dessin" de l'output
-//(elle prend en paramètre le numéro de ligne du début et de la fin du dessin dans le fichier)
+// Fonction affichant l'ASCII-art.
+// Elle prend en paramètre le numéro de ligne du début 
+// et de la fin dudit ASCII-art dans ./ressources/art.
 void art(int begin, int end) {
 	FILE *pepe = fopen ("./ressources/arts", "r");
   char line[255];
@@ -33,7 +65,7 @@ void art(int begin, int end) {
 }
 
 
-//Fonction affichant la partie texte de l'output (même fonctionnement que la fonction au-dessus)
+// Idem pour les dialogues.
 void text(int intro, int outro) {
 	FILE *sans = fopen ("./ressources/verbose", "r");
   char vers[255];
@@ -47,8 +79,7 @@ void text(int intro, int outro) {
 }
 
 
-//Pour afficher l'art + le texte (chaque cas est défini par la zone)
-//OBSOLÈTE, À DIVISER EN DEUX POUR SÉPARER LE TEXTE DE L'IMAGE
+// Affiche le dialogue en fonction de la zone.
 void dialogue() {
 	switch (sequel) {
 		case 42:
@@ -74,8 +105,7 @@ void dialogue() {
 }
 
 
-//Pour afficher l'art + le texte (chaque cas est défini par la zone)
-//OBSOLÈTE, À DIVISER EN DEUX POUR SÉPARER LE TEXTE DE L'IMAGE
+// Idem pour l'ASCII-art.
 int tableau() {
 	switch (sequel) {
 		case 42:
@@ -98,8 +128,8 @@ int tableau() {
 }
 
 
-//Pour afficher la suite de la description, chaque cas est défini par une variable
-//elle-même ayant la valeur de la zone lors de chaque déplacement puis de elle-même + 100.
+// Incrémente sequel de 100 en 100 pour permettre à dialogue() 
+// d'afficher la suite des paragraphes quand il le faut.
 int suite() {
 	if (strcmp(input, "suite") == 0) {
 		sequel = sequel + 100;
@@ -109,6 +139,7 @@ int suite() {
 }
 
 
+// Réinitialise sequel et affiche le début du dialogue.
 void back() {
 	if (strcmp(input, "retour") == 0) {
 		sequel = zone;
@@ -118,6 +149,7 @@ void back() {
 }
 
 
+// Appelée en cas d'évênement imprévu (message d'erreur).
 void wrong() {
 	sequel = zone;
 	tableau();
@@ -125,7 +157,12 @@ void wrong() {
 }
 
 
-//Fonction appelée au début du jeu, finie à partir du premier tableau
+/*FIN DES FONCTIONS "MOTRICES"*/
+
+
+// Cette fonction est un cas à part  : elle est appelée au lancement
+// du logiciel et affiche l'écran titre. Elle n'est plus appelée par
+// la suite.
 int start() {
 	printf("\e[1;1H\e[2J");
 	art(0, 17);
@@ -152,7 +189,12 @@ int start() {
 }
 
 
-//Affiche l'art et la description de la zone actuelle
+/*Commandes
+Ces fonctions correspondent à des commandes effectuables
+par l'utilisateur.*/
+
+
+// Affiche l'ASCII-art et la description de la zone actuelle.
 void inspect() {
 	if (strcmp(input, "inspecter") == 0) {
 		sequel = zone;
@@ -162,6 +204,7 @@ void inspect() {
 }
 
 
+// Affiche l'aide.
 void help() {
 	if (strcmp(input, "aide") == 0) {
 		sequel = 100;
@@ -191,9 +234,9 @@ void help() {
 }*/
 
 
-//Cette fonction change la valeur de la variable "zone" de manière à
-//simuler un déplacement vers le nord du point de vue du joueur.
-//on limite ça à 0 pour éviter de sortir de la map
+// Cette fonction change la valeur de la variable "zone" de manière à
+// simuler un déplacement vers le nord du point de vue du joueur.
+// Limité à 0 pour ne pas "sortir" de la carte.
 void north() {
 	if (strcmp(input, "nord") == 0) {
 		if ((zone - 10) >= 0) {
@@ -207,7 +250,8 @@ void north() {
 	}
 }
 
-//Même chose pour le sud
+
+//Idem pour le sud
 void south() {
 	if (strcmp(input, "sud") == 0) {
 		if ((zone + 10) <= 99) {
@@ -221,7 +265,8 @@ void south() {
 	}
 }
 
-//Même chose pour l'est
+
+//Idem pour l'est
 void east() {
 	if (strcmp(input, "est") == 0) {
 		//La ligne suivante ne me plaît pas mais j'ai pas encore trouvé mieux :/
@@ -236,10 +281,11 @@ void east() {
 	}
 }
 
-//Même chose pour l'est
+
+//Idem pour l'ouest
 void west() {
 	if (strcmp(input, "ouest") == 0) {
-		//Idem que pour l'ouest
+		//Idem que pour l'est
 		if (zone == 0 || zone == 10 || zone == 20 || zone == 30 || zone == 40 || zone == 50 || zone == 60 || zone == 70 || zone == 80 || zone == 90) {
 			printf("Vous ne pouvez pas aller plus loin.");
 		} else {
@@ -252,7 +298,11 @@ void west() {
 }
 
 
-//Liste des commandes faisable par l'utilisateur
+/*FIN DES COMMANDES*/
+
+
+// Liste des commandes faisable par l'utilisateur
+// (pour éviter d'avoir un énorme if/else).
 void actions() {
 	help();
 	inspect();
@@ -267,16 +317,16 @@ void actions() {
 
 //Fonction principale, "moteur" du jeu
 int main() {
-	start();	//Fonction affichant l'écran titre et démarrant le jeu
+	start();	//Écran titre
 	int k = 0;
-	while (k < 1) {	//cette boucle est ensuite répétée infiniment :
-		fgets(input, 12, stdin);	//on récupère la commande (input)
-		purify(input, 12);				//on la "purifie" (voir début du code)
-		printf("\a \e[1;1H\e[2J");	//ici on fait un bip + on nettoie le terminal
-		actions();	//et enfin on effectue la commande
+	while (k < 1) {	//Cette boucle est ensuite répétée infiniment :
+		fgets(input, 12, stdin);	// on récupère l'input
+		purify(input, 12);				// on enlève le \n
+		printf("\a \e[1;1H\e[2J");// on fait un bip et on nettoie le terminal
+		actions();	// et enfin on effectue la commande.
 		if (strcmp(input, "quitter") == 0) {
-			return k = 1;		//cette condition brise la boucle et fait quitter le jeu
+			return k = 1;		// Cette condition brise la boucle et fait quitter le jeu.
 		}
-		printf("[Zone:%d | Sequel:%d]> ", zone, sequel);		//on imprime le ">" en bleu pour faire comprendre qu'on attend une action de la part de l'utilisateur
+		printf("[Zone:%d | Sequel:%d]> ", zone, sequel);		// Affiche le ">".
 	}
 }
